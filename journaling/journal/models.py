1937eb2dd88724409312b7d47c24e17084d646ee
Rubén Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2025-09-01 21:59:38 +0200

    Added AI feedback to entry from Gemini

--- journaling/journal/models.py
+++ journaling/journal/models.py
@@ -3,5 +3,6 @@ from django.db import models
 # Create your models here.
 class Entry(models.Model):
     text = models.TextField()
+    ai_analysis = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- journaling/journal/views.py
+++ journaling/journal/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Entry
+from .gemini_requests import generate_ai_analysis
 
 
 # Create your views here.
@@ -7,7 +8,8 @@ from .models import Entry
 def home(request):
     if request.method == "POST":
         text = request.POST.get("text")
-        Entry.objects.create(text=text)
+        ai_analysis = generate_ai_analysis(text)
+        Entry.objects.create(text=text, ai_analysis=ai_analysis)
         return redirect("home")
     entries = Entry.objects.all().order_by("-created_at")
     return render(request, "home.html", {"entries": entries})
