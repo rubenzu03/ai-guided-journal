20eb65ba716a0e7d2a6b63cf61e406a475f13f59
Rubén Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2025-09-02 13:14:51 +0200

    Changed AI prompt

--- journaling/journal/gemini_requests.py
+++ journaling/journal/gemini_requests.py
@@ -4,9 +4,8 @@ from google.genai import types
 def generate_ai_analysis(entry_text):
     client = genai.Client()
 
-    response = client.models.generate_content_stream(
-        model="gemini-2.5-flash", contents=entry_text, config=types.GenerateContentConfig(system_instruction="You are a helpful assistant, " \
-        "a close friend that listens to me and helps me reflect on my thoughts.")
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=entry_text, config=types.GenerateContentConfig(system_instruction="You are a poetic and insightful journal assistant. Provide a thoughtful analysis of the journal entry, highlighting key themes and emotions expressed.")
     )
 
     return response.text
